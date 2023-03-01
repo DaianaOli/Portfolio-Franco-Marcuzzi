a569a4effea960126ef3311ea2fd7b9c5d91cb84
Luz Daiana Olivares <97685217+DaianaOli@users.noreply.github.com>
2023-03-01 14:44:35 -0300

    creacion de estructura-contact y form

--- portfolio-fm/src/App.jsx
+++ portfolio-fm/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import LandingPage from './components/LandingPage/LandingPage'
 import NavBar from './components/NavBar/NavBar'
+import Contact from './components/Contact/Contact'
 
 function App() {
   return (
     <div className="App">
     <NavBar/>
     <LandingPage/>
+    <Contact/>
     </div>
     
   )
