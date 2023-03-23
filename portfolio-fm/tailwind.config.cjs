3793545fa8457c0e5d9081f1abe298f262ca18e3
DarioMarcuzzi <darik95@hotmail.com>
2023-03-23 15:49:50 -0300

    bg-noise transition

--- portfolio-fm/tailwind.config.cjs
+++ portfolio-fm/tailwind.config.cjs
@@ -1,14 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
       colors: {
         dark: "#1a202c",
+      },
+      backgroundImage: {
+        noise: "url('/src/assets/noise-transparent.png')",
+      },
+      animation: {
+        bganimation: "bganimation 2s infinite",
+      },
+      keyframes: {
+        bganimation: {
+          "0%": { transform: "translate(0,0)" },
+          "10%": { transform: "translate(-5%,-5%)" },
+          "20%": { transform: "translate(-10%,5%)" },
+          "30%": { transform: "translate(5%,-10%)" },
+          "40%": { transform: "translate(-5%,15%)" },
+          "50%": { transform: "translate(-10%,5%)" },
+          "60%": { transform: "translate(15%,0)" },
+          "70%": { transform: "translate(0,10%)" },
+          "80%": { transform: "translate(-15%,0)" },
+          "90%": { transform: "translate(10%,5%)" },
+          "100%": { transform: "translate(5%,0)" },
+        },
       },
     },
   },
@@ -18,5 +36,7 @@ module.exports = {
       textColor: ["dark"],
     },
   },
+
+  keyFramg: {},
   plugins: [],
 };
