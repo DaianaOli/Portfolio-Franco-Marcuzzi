6fc13eed71f32d6bf9731dca91db0945153b8f14
DarioMarcuzzi <darik95@hotmail.com>
2023-04-03 15:41:24 -0300

    experiencia-scrollLoading-Data-BGNoise

--- portfolio-fm/src/components/Experiencia/Experiencia.tsx
+++ portfolio-fm/src/components/Experiencia/Experiencia.tsx
@@ -1,9 +1,147 @@
-const Experiencia = ()=>{
-    return( 
-        <div>
-            <h1>Experiencia</h1>
-        </div>
-    )
+import { useEffect, useState } from 'react';
+import { data } from '../../data.json';
+
+
+
+
+interface ExperienciaLaboral {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  fecha_inicio: string;
+  fecha_fin: string;
+  imagenes?:Imagenes[];
+  imagenes2?: Imagenes[]
 }
 
-export default Experiencia
+interface Imagenes {
+  id: number;
+  imagen: string
+
+}
+
+
+
+function Experiencia({ experiencia }: { experiencia: ExperienciaLaboral }) {
+  const [hover, setHover] = useState(false);
+  const [statePhotos, setStatePhotos] = useState(0)
+ 
+  
+  
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (!hover) {
+        setStatePhotos((prevCounter) => {
+          setHover(true)
+          return prevCounter === 1 ? 0 : 1;
+        });
+      } else {
+        setHover(false)
+      }
+
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [hover]);
+
+  
+
+
+
+  return (
+    <div  className="flex flex-col h-screen w-full justify-center "
+   >
+      <h1 className=" mx-4 text-4xl text-gray-300">{experiencia.nombre}</h1>
+      <div className="flex items-center  m-4 h-3/5"
+       >
+        <div data-aos="fade-right" className="flex flex-col text-3xl gap-5   text-gray-600  w-2/4">
+          <h1>{experiencia.descripcion}</h1>
+          <div className="mt-2">
+            <p data-aos="fade-right">
+              Fecha de inicio:{' '}
+              <span className="text-white">{experiencia.fecha_inicio}</span>
+            </p>
+            <p data-aos="fade-right">
+              Fecha fin:{' '}
+              <span  className="text-white">{experiencia.fecha_fin}</span>
+            </p>
+          </div>
+        </div>
+        <div className=" flex flex-col w-2/4 h-full  justify-center items-center group">
+          <div data-aos="fade-right" className=" border-4 shadow-lg grid grid-cols-4 content-center   group-hover"
+         >
+            
+            {data.fondo.map((e, index)=> {
+              return (
+                <div className={`  object-cover transition ease-in-out [transform-style:preserve-3d] ${hover ? "[transform:rotateY(180deg)]" : ""}
+                  duration-1000 delay-[var(--delay)]`}
+                style={{
+                  transitionDelay: index * 0.1 + 's',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                   {e.imagen && (
+                    <div key={e.id}>
+                      <img src={e.imagen} alt="imagen" />
+                    </div>
+                    )}
+                 
+                  <div className='absolute inset-0 h-full w-full  [transform:rotateY(180deg)]  [backface-visibility:hidden]'
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}>
+                    {
+                      statePhotos === 0 ? <>
+                      {experiencia.imagenes && experiencia.imagenes.length > index && (
+                    <img className="" src={experiencia.imagenes[index].imagen} alt="imagen" />
+                  )}</>: <>
+                  {experiencia.imagenes2 && experiencia.imagenes2.length > index && (
+                    <img className="" src={experiencia.imagenes2[index].imagen} alt="imagen" />
+                  )}</>
+                    }
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Experiencias() {
+  const { experiencia_laboral } = data;
+
+  const scrollToTop = () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'auto' 
+    });
+  };
+
+
+
+  useEffect(() => {
+    scrollToTop();
+
+  }, []);
+
+  return (
+    <div className=" relative flex flex-col font-serif w-full h-full items-center">
+      <p className="flex mt-20 h-auto  text-6xl text-white">
+        Experiencia Laboral
+      </p>
+      
+      {experiencia_laboral.map((e: ExperienciaLaboral) => (
+        <Experiencia key={e.id} experiencia={e} />
+      ))}
+    </div>
+  );
+}
+
+export default Experiencias;
